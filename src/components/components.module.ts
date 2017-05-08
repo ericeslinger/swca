c504b4d45c771be47d9ab7be2a772acd87368591
Eric Eslinger <eric.eslinger@gmail.com>
2017-05-08 10:52:35 -0700

    added browser platform
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CriticalRoller } from './critical-roller/critical-roller';
 import { DieChooser } from './die-chooser/die-chooser';
 import { NIcons } from './n-icons/n-icons';
+import { IonicModule } from 'ionic-angular';
 
 import { CommonModule } from '@angular/common';
 
@@ -13,6 +14,7 @@ import { CommonModule } from '@angular/common';
   ],
   imports: [
     CommonModule,
+    IonicModule,
   ],
   exports: [
     CriticalRoller,
